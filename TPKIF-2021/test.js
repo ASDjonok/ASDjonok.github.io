9a43c6e55f05e2ef927113717ebcbef9a98f2c0d
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2021-09-25 14:07:31 +0300

    add css and js

---
+++ TPKIF-2021/test.js
@@ -0,0 +1,12 @@
+alert("Hello world!");
+console.log("in JavaScript");
+// let age = prompt('Сколько тебе лет?', 100);
+let age = prompt('Сколько тебе лет?');
+alert(`Тебе ${age} лет!`);
+/*const something = document.getElementById('paragraph1');
+
+something.style.cursor = 'pointer';
+something.onclick = function() {
+    // do something...
+    alert (1)
+};*/
