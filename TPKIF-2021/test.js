9b5b1f2d935e33aff27b22c63613d1dec9dcdd40
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2021-10-09 12:46:31 +0300

    update css, js

--- TPKIF-2021/test.js
+++ TPKIF-2021/test.js
@@ -1,12 +1,27 @@
-alert("Hello world!");
-console.log("in JavaScript");
+// alert("Hello world!");
+window.console.log("in JavaScript");
 // let age = prompt('Сколько тебе лет?', 100);
-let age = prompt('Сколько тебе лет?');
-alert(`Тебе ${age} лет!`);
-/*const something = document.getElementById('paragraph1');
+/*var age = prompt('Сколько тебе лет?');
+if (age > 0) {
+    // var newAge = age + 1;
+    let newAge = age + 1;
+}
+alert(`Тебе ${newAge} лет!`);*/
+
+// const something = document.getElementById('paragraph1');
+// const something = document.getElementsByClassName('p-js')[0];
+const something = document.getElementsByTagName('div')[0];
+
+/*something.style.cursor = 'pointer';
+something.onclick = function() {
+    // do something...
+    alert (';)')
+};*/
+
+// const something = document.getElementById('paragraph1');
 
 something.style.cursor = 'pointer';
 something.onclick = function() {
     // do something...
     alert (1)
-};*/
+};
